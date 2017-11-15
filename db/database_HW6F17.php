c8e1693ed24b7ae8ebfba082ba1da2aff342d870
nati-natnael <natnaelger@gmail.com>
2017-11-15 13:33:07 -0600

    [DB]: add data base operations

---
+++ db/database_HW6F17.php
@@ -0,0 +1,8 @@
+<?php
+      $db_servername = 'cse-curly.cse.umn.edu';
+      $db_port = 3306;
+      $db_name = 'F17CS4131U91';  //Note is a  number, you can find it on moodle
+      $db_username = 'F17CS4131U91';    //xxx is the number assigned on the moodle
+      $db_password = 10503;  // provided in moodle.
+?>
+
