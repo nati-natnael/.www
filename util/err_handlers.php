4194ec1b02aa59c43a4e541b6de747906114cdb3
seifu003 <seifu003@umn.edu>
2017-11-19 21:26:24 -0600

    change msg container from span to div

--- util/err_handlers.php
+++ util/err_handlers.php
@@ -5,15 +5,15 @@
     * :param $errMsg: description of error
     */
     function errMsg($errMsg) {
-       $msg  = "<br><span style='color: red;'>";
-       $msg .= "<img src='imgs/err_img.png'
-                          alt='Check Mark Image'
-                          style='width: 1.1em; height: 1.1em;'> ";
-       $msg .= "<span style='vertical-align: top'>";
+       $msg  = "<div style='color: red; margin-top: 0.5em;'>";
+      //  $msg .= "<img src='imgs/err_img.png'
+      //                     alt='Check Mark Image'
+      //                     style='width: 1.1em; height: 1.1em;'> ";
+       $msg .= "<span style='padding: 0.1em;'>";
        $msg .= $errMsg;
        $msg .= "</span>";
-       $msg .= "</span>";
-    
+       $msg .= "</div>";
+
        return $msg;
     }
 ?>
