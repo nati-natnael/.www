2b22cf2e29539fdd1a61c7eba61be8cc27349154
seifu003 <seifu003@umn.edu>
2017-11-19 21:26:51 -0600

    add password validator

--- util/string_utils.php
+++ util/string_utils.php
@@ -7,15 +7,15 @@
     function capitalizeWords($phrase) {
        $formatted = "";
        $words = explode(' ', $phrase);
-    
+
        foreach ($words as $word) {
            $tWord = trim($word, ' ');
            $formatted .= " " . ucfirst($tWord);
        }
-    
+
        return $formatted;
     }
-    
+
     /**
     * Validate input string
     *
@@ -23,24 +23,46 @@
     */
     function validate($string) {
        $pattern = "/^[a-z0-9]+$/";
-    
+
        if (empty($string)) {
            return false;
        }
-    
+
        $lowerString = strtolower($string);
        $pieces = explode(" ", $lowerString);
-    
+
        foreach ($pieces as $piece) {
            $match = preg_match($pattern, $piece);
            # echo "String: $piece | Match: $match <br>";
-    
+
            # If any string piece doesn't match stop
            if (!$match) {
                return false;
            }
        }
-    
+
        return $match;
+    }
+
+    /**
+     * validate password.
+     *
+     * Note:
+     *  password must be at least 4 characters long
+     *
+     */
+    function passwordValidate($password) {
+      $passLen = 4;
+
+      if (empty($password)) {
+        return FALSE;
+      }
+
+      $chars = str_split($password);
+      if (count($chars) < $passLen) {
+        return FALSE;
+      }
+
+      return TRUE;
     }
 ?>
